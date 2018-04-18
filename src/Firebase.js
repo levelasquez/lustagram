131493230310bc0e51a33bc6d833e1fc72b7cef9
levelasquez <velasquezleonardo1@gmail.com>
2018-04-17 21:15:36 -0300

    firebase integration

---
+++ src/Firebase.js
@@ -0,0 +1,12 @@
+import * as Firebase from 'firebase'
+
+const config = {
+  apiKey: 'AIzaSyBofSHwKh8jhRC6s3xSbxGfqrjevjG2yOs',
+  authDomain: 'memegram-rn.firebaseapp.com',
+  databaseURL: 'https://memegram-rn.firebaseio.com',
+  projectId: 'memegram-rn',
+  storageBucket: 'memegram-rn.appspot.com',
+  messagingSenderId: '357090886974',
+}
+
+Firebase.initializeApp(config)

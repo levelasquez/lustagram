341a0aab10782590197407a831b3b91cc11fa25f
levelasquez <velasquezleonardo1@gmail.com>
2018-04-18 22:02:17 -0300

    redux-saga effects takeEvery and call

--- src/Sagas.js
+++ src/Sagas.js
@@ -1,3 +1,16 @@
-export default function* test() {
-  console.log('rest')
+import { takeEvery, call } from 'redux-saga/effects'
+
+import { Authentication } from './Firebase'
+
+const firebaseRegister = (email, password) =>
+  Authentication.createUserWithEmailAndPassword(email, password)
+    .then(success => console.log({ success }))
+    .catch(error => console.log({ error }))
+
+function* register({ payload: { email, password } }) {
+  yield call(firebaseRegister, email, password)
+}
+
+export default function* sagas() {
+  yield takeEvery('REGISTER', register)
 }
